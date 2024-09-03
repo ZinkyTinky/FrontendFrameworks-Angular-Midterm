7d300dd152ba18d6bf9c31e8a3b8304a482817d7
Zian Jacobsen <25855271@sun.ac.za>
2024-09-03 15:52:02 +0200

    Completed code for the jobs component.

--- src/app/components/jobs/jobs.component.ts
+++ src/app/components/jobs/jobs.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { JobServiceService } from 'src/app/services/job-service.service';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
   styleUrls: ['./jobs.component.css']
 })
-export class JobsComponent {
+export class JobsComponent implements OnInit {
 
+  jobsList: any = [];
+
+  constructor(private jobService: JobServiceService) { }
+
+  ngOnInit(): void {
+    this.jobService.getAllJobs().subscribe(list => {
+      this.jobsList = list;
+    })
+  }
 }
